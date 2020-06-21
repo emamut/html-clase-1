7b2a7d87b6366d83b2f749a8fbabf5cb0bded5a2
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-06-20 20:08:38 -0500

    starts questions behavior

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var app = new Vue({
   el: "#app",
   data: {
-    questionsArray: []
+    questionsArray: [{}],
+    activeQuestion: 0,
+    activeSection: 0
   },
   mounted() {
     var self = this
@@ -9,5 +11,19 @@ var app = new Vue({
     axios.get('./js/data.json').then(function (response) {
       self.questionsArray = response.data.questions
     })
+  },
+  methods: {
+    checkActiveQuestion: function () {
+      if(this.activeQuestion < this.questionsArray.length - 1)
+        this.activeQuestion ++
+      else
+        this.activeSection ++
+    },
+    isActive: function (sectionID) {
+      if (sectionID == this.activeSection)
+        return 'd-block'
+      else
+        return 'd-none'
+    }
   }
 })
